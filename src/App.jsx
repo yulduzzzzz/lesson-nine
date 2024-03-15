1017501d345f0c01256d2f281c4e990ad58eaf2e
yulduzzzzz <yulduzabduvahidova@gmail.com>
2024-03-15 11:41:32 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,183 @@
+import React from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <div className="container">
+        <header className='header'>
+        <h1 className='title'>How it Works</h1>
+        <h1 className='title'>The Membership</h1>
+        <h1 className='title'>The Blog</h1>
+        <h1 className='title'>Our Story</h1>
+        <button className='btn' >Get Started</button>
+        </header>
+        <header className='header-one'>
+          <h1 className='header-title'>Aro turns screen <br /> time into family time</h1>
+          <p className='text-header'>Aro helps families put down their phones to connect, relax, and recharge</p>
+        </header>
+        <main className='main'>
+         <div className="main-one">
+         <h1 className='main-title'>Introducing Aro</h1>
+         <p className='main-text' >Aro is the first in-home digital wellbeing solution <br /> designed for families. The Aro platform ensures you <br /> don’t miss out on what’s most important by making <br /> it easy for everyone to put down their phones and <br /> be fully present</p>
+         </div>
+          <img className='img' src="https://s3-alpha-sig.figma.com/img/cc0c/3f26/4ad8c95034293d452bf1089242477f57?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fXJpw3ki5zqPKESQ0SdExarmZZ41ReRluJ27nnz56qZB4IwESbxaJY7eAXaSnm5BYuGN-SgTsupmsWP6X2vSK86qYd2eXR-9K9rPefKYxlhVGCVAPIrbyctsdpkvGRasY9q6BRgnZM0jph0-QHsPNXIIteuGtLoPXMXhvzc3VTuSxCrGYAO4xNg2mCEIzw7cBtQUXqsxkgBrYkfR2dn-Y5~W-O-C-2V9rKD4wYd9RhuPjJxIVU7bMFiwzxKH6q6zz3VP79NWWWCRRBDfmg2Aqg3T0aklj-Ss4e9ROzJiH9XeZg2iTzP5-87AYPGqwmXeoYub97lnCNlbM7P1aKh3pA__" alt="" />
+        </main>
+        <div className="main-two">
+         <button className='btn-one'>Join Now</button>
+         <div className="main-main">
+         <h1 className='main-title-two'>How it works</h1>
+         <div className="liner"></div>
+         </div>
+         </div>
+         <div className="main-three">
+          <div className="main-title-one">
+          <h1 className='main-title-three'>Spend quality <br /> time together</h1>
+          <p className='p-one'>We only get so much time with our kids--18 years goes <br /> by quickly. Aro helps both parents and kids put down <br /> their phones when it’s family time so you don’t look <br /> back wishing you spent more time with your family and <br /> less time on your phone.</p>
+        <h1 className='price'>75%</h1>
+        <h3 className='main-title-four' >of the time we spend with our kids in our lifetime will be <br /> spent by age 12.</h3>
+          </div>
+        
+        <div className="main-img">
+          <img className='img' src="https://s3-alpha-sig.figma.com/img/0e80/f203/8f1dd3df5294fd1b41f855396ad2fcb5?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EmzBvueePOuy9vai2-XewInL6Zf3O-LN1EkQjPgMOw2gAylDyM9kEMuosPLY3UTtTGK9gOcXoMxsv4VKmtaKlP7y-ewBv3OwT7ochTePLL9ZffFPVFUDXb0ZHrizMKxjPgjjuUQLCJlWDFHPUHxVbOPa8TWWr-a1dO6Tmksb1unA55Qyiz7NDDZjxWACIzBLldTqqGmcJK1TLvGpkLGRwAuXmjMBx5SDAud8nmxOiN8RGwLkmqiNe32qLi-uqUUMtH13teSEGBbu3ePx93H4LEsTQJQPjrHEyTRVzJ3xIaWROqXB2aYwEf4ZOqEJy-rK4a5xqhKVhC3iXq0nsvaYvg__" alt="" />
+        </div>
+         </div>
+         <h1 className='title-three'>Featured In</h1>
+         <div className="box">
+          <img className='one' src="https://s3-alpha-sig.figma.com/img/65c9/99b0/2f98df6a4c8a89d51ae14dfbb930f4f7?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Uv4Byd-FReCJZPa8Rwe5EEl6~zvA~zWWMXkYkFFiK~o-nWYdu5E8hdYeTOuI0ZJWIcNihrZ69BhAnSSQOdfPtKMYPp4lmVLeYWhx-GNxaoxH~nh6kbABB22obryICwcDUg4eE~LQl2U8sj2tzv3MeBBvh0LeAVNUlIuXu~XzMBlQA6DUEmXYCC2rfD902PuGr1kOAB~-FeNtR60lYq8bOXFVHTWbhGwjR3M~6k-BBMWufVghwQesG5y2IadJD-TW00niqGT64JPuPJpDz7xOd6XSi~Y-VApjR3NFrX-v3FTZOSWdoWpBlyLKZtoDOmNIy7c5D27Tg8ffHkTmphJT2g__" alt="" />
+         <img  className='one'  src="https://s3-alpha-sig.figma.com/img/be02/0f45/5428aa8e04e5d19c87697fba6a004654?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iZYtBPGjr4~xv2eg9i5wxGw7rjEo~QvJJjZkTJE93SZQVUWfYBObVCXfI3WLxoaNOCIK8CW2jWYzaiy6vf5lbL1Uzpx1pRW2o~hVjegD3XKdNUTZxrJ6PCDTVfiRfjFt592HIEmcJ7Tl3izBpmQSFBVRZkM75~XoW~rssDPs9gIMvoNAfCWRwXz8JPhtqswAlhALAUqrzcT-Do1eHRQZDNieAOuLDKOv0KhWc-P7qKZzkkHPyXiPUk1xr76gyStWoY0UW3l1HCQ7ps4lnNcDbdYXY17SZ9yOSJzX0FTzyakAriE6Cd~RVhnIuLgSgzYYWRpMa18gdm-ReWkx-irVdw__" alt="" />
+        <img  className='one' src="https://s3-alpha-sig.figma.com/img/7885/bf7d/2caca40298e5d29222fa287de95fc428?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=L4qiy6b-7CTaE2X6HtXRDV73Y4xM5vvVBdcvdncoaocjceH3XbPxjbzWlobFTwfFQVd4D1PmmaflJAaI6Q5XNGgVCBfOKL43xrk3EBIUIUcrv0JD9fY9QoA8VFUcGbGXitoQIZvtzLFo8hgHdS7roNj8i0m0TgJZBP956QohZm4ns5aXmQ9ML9OIIa8FuWllauByOC8oyZXrb6Ycw62jtZVSFckkjQ7SV4CmNIJwLdqkrf-IAbbrh-1KcThp-MvpvCHrNf1UvIcU3fxRfPraMRJpCWNeLAciVzOgRmtLTKIcAaY0NEaG0-6Fg1EUqUJEfiohJxjnLtYkKmOyChMIcw__" alt="" />
+        <img  className='one' src="https://s3-alpha-sig.figma.com/img/4d18/d237/702247e56acd4d6688d81fba8a01e476?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Tq3Xa0PP29ZC89Rgma-k9lmkLSXDGcxN-dd7D2FTiP~DJfrW8E9k43bxvkS~YkSnE2V8apf7LWkxDOJrxRNcwwexLZQ28LhPj6DKADeoUQf5JEYB39jna0yOVrljRzi-DLjKWDf2O3L8EdsyhbxJxOnLTMoFmwRGe71g8u7eMtrH1Yf-IMUCBHE-3MP9ll5rO0bv6ct9wrXh4qAesAM8fLWEfJE6ptJu~BOLwet~r~46Igv5n6yLC4cz8b04qJTUfDJVc6-Of8-pn6Tb6AB~FcS1wMQ2t20eQRkoSIburw09~O0G6rBVVfyY3F-D3p7unyjc~gmyMZ1AAUMEkvcPJw__" alt="" />
+      <img className='one' src="https://s3-alpha-sig.figma.com/img/7909/526a/6a9808083b7aa4034151fd17f86a04bc?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CEl9F4rtxNDXbjM-~kJL35cfxXJpIAQMgMt7~YUihtEE-SCW2T-2kxHn3x3T9oT0~5qQ5mzEDFfO82mPZDqqbn1Pj1avZ9dSxHptrm7uorYQafPcWMdGe7~mP2DKCpUCcK-u1U-Ca5lBDDpXcA3LSsZzoj5zq2nZEbrSSdcBhSmX~6BAOJ5P9~poTEejsRMajPXikAORT5FtBZr0sS7OXrxuHV7avsS24GnBSzPjpz16nPVAxNUXGpaPAZGWRohhth3ciHQLCfkYNZDZt6d8mAxgRpR9qhruCkhvlZtrZLiI-MKHmcxNWopzJN2gOGCyhpluEngoeeJbeEB6kGNC3g__" alt="" />
+         </div>
+         <img  className='two' src="https://s3-alpha-sig.figma.com/img/b370/525b/73aa86a0d7b9011ba871e74bb7052ef3?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EOICTRvkWltfwUjsX70TUCXyvd98XghSga1TJRbPATBBKHJd04MJDnWxXYrWX-nZcJibYdfTgI53D2hDLK4Yt~FsV1qDkyRXNelpHKKJ~i-uwxQ-eN8Awu3B1ik7rWgphyH6hGoIwMKQj5KpxbjuScLckN2rbK7M2IDOnFS3jIQmnE9C6iS00s7Cdd1ZluhnfH0altP5iGA8rFZ4c66BlV9JipZ3tLZjAn0HqVwNmw8q5hKmXwP1NbWsl-0l5gKV4CT7hGIYxPsTaKf7yHlJI~xU0AImolDNPvu0YeZNNgyoPq90dAPyJ08T960uk6GybWyEICtZkROVYKiXohnB-w__" alt="" />
+      <div className="box-main">
+        <h1 className='t' >See Aro in Action</h1>
+        <p className='pp' >Aro helps families put down their phones to connect, relax, and recharge.</p>
+     <div className="box-box">
+      <h1 className='tt' >Watch Now</h1>
+     </div>
+      </div>
+     <div className="main-medium">
+      <div className="main-mediumm">
+      <h1 className='ttt' >The Aro Experience</h1>
+      <p className='pppp' >Aro is so much more than an app and box for you phone. It’s about what happens once you put down your phone. Aro lets parents and kids connect by creating an environment where phones aren't a distraction.</p>
+     <h3 className='tttt'>See How it Works</h3>
+     <div className="liners"></div>
+      </div>
+     <div className="main-main-medium">
+<img className='img-onee' src="https://s3-alpha-sig.figma.com/img/15da/b63d/8c92542ec6e1b319193e7ad2f281345f?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hH9iN2uo~pNBLEeaSJHbeJEOdbECFqMg1LCG92lqt37PVhfnqLyMRTyDyJc7NF~WjBwoOF-v-DQus0dozNiIkNJt8xm4w5LChssFTVrnCps42CIb-tpwaWZbssgf1TDhDjy2VZFwQfR2dcGzE~OxBUUWXjzpD2DQF1rTPtWKGSIShSho~PoNhvwe2igTG7wZ2lurtKvEEU6jcKoO-aNPpRyx5~dtS-sGbUOu7ikKcgd06cUILrziI0fv~KK7SG1cIgEbERu5ISL2Cv6ju08y5vhyEMDEP773Am-b-7M6ZKpUxCOScBsTmuub6uRutWZtodppfFEkT6nn2NXnpUc5Gw__" alt="" />
+     </div>
+     </div>
+<div className="main-medium-one">
+  <div className="main-medium-onee">
+    <img className='img-img' src="https://s3-alpha-sig.figma.com/img/2c05/0779/d96a0bb76cc79e9d6ff516d202ac84ec?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qrMieJDYMLcGRwcyxM1hiRSG~4bT6mg33JQjfLGDslbW0iyct2UKt5ZaCXxAgmbss9CzYmY3LMvpJ96H~bk-Cs5wQ7zbCg00W4QMMGJtDCCPwtaQXK-nswzcNOjW~c6JIjgs9t6G9esdvRdkeRpecNNfnaxXK~bAFxTA8fEeY0SsqAJmb28MDw7rgEXhty~JDK5NT1KPPhOBqaheGIH16fZ9Bbx~iiy0VanJzFZ0G7ih7TlJ1Ui0pAEhQHt~Mg8EDwVKq-W63Zo2gZzdDq7mT8wSdwWasRYpSbTVRyRM9W0LmFkYNmIbuAyoT8wyAZhwm0ypUKyI9uHgZTuMwzrFow__" alt="" />
+  </div>
+  <div className="main-medium-two">
+    <h1 className='main-t' >The Aro App</h1>
+    <p className='main-pppp' >The Aro app gamifies the experience of being off your phone. The app automatically connects to the Aro Home device, measures the time you spend away from your phone, reminds you to take breaks from your phone, shows you data to improve your phone habits, and even lets you connect and compete with others.</p>
+  <h4 className='h' >See How it Works</h4>
+  <div className="linerss"></div>
+  </div>
+</div>
+<div className="main-over">
+  <button className='btn-three' >Get Start</button>
+</div>
+<div className="box-over-one">
+  <div className="main-p">
+  <p className='over-p'>“I don’t want my kids constantly seeing me with my head down to be normal. Aro gives us an invitation to put our phones away and allows us to be more present with each other.”</p>
+<h3 className='over-title'>Shelly McIntosh, Mom of 2</h3>
+  </div>
+<div className="main-p-p">
+<p className='over-pp' >“Aro holds us accountable for the time that we spend every day. Instead of looking down at our phones, we’re looking into our kids’ eyes, we’re playing with them, and we’re shaping our family in a more loving and powerful way.”</p>
+<h2 className='over-h' >Clay McInnis, Dad of 2</h2>
+</div>
+<div className="main-p-p-p">
+<p className='over-ppp' >“We’ve always had our kids keep their devices outside in the living room before they go to bed, but now our daughter loves putting her phone in Aro at night so she gets credit for her time away.”</p>
+<h3 className='over-titlee' >Erin Burkhalter, Mom of 2</h3>
+</div>
+</div>
+<div className="big-main">
+  <div className="main-over-two">
+    <img  className='img-over' src="https://s3-alpha-sig.figma.com/img/5521/9dd7/b6389ea2546eb9402625be3ef9e29b8f?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Jh30VLu7rgnoLznWJ-z406fNh0noziGIDlBrtNPoig2Uvf1LX5dhohxNXMbmadINeZlbblIp8Vcc-KOv9VLwN7PWFPI932HjWAJDJh-JGZ54wL13YCF9jzRzMxtI4GS8B3vCtSbMRB8J-BnM0LWs6w29Nb0u~FcjvuV-DduLLm16BObcEJAmou3sVpl3QMC8gJ7ovluDHYD3hrEbyqNXyYnK1wZ~25OpmnyCH4-EMy55Zukc~s0-bADCk-FyHFEkNBArdjRpfc9lnhnVxmBeULMMPKbFQlUow6kRf7F88Kv3qqZoKdqR~jev~gk2h6MAs2roCGgSwj1pxV2ZSo3fIA__" alt="" />
+ <h2 className='t-p' >Creates space from your phone</h2>
+ <p className='p-tt' >Unlike other solutions designed to reduce screen time, the Aro platform combines an app and a physical device to create distance between you and your phone so you can have quality time without the distraction of your phone.</p>
+  </div>
+  <div className="main-over-two">
+    <img  className='img-over' src="https://s3-alpha-sig.figma.com/img/799b/2d14/2b49ab0f2d8155a92d9424eaf99bdaf6?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=n8s-HBBfMugxoU0IZqew15WG0X25Z2DCI7eDTIL3gts42uJkXQ2E-j-~AKVaZLKMct61L6BXOphF6-lcs50mMlYtLurZL6SCLgjGjKg7RSqCDLI-a7Fk2SB6QqMx69rY5nVI3wsCOaJKRT4gaGF~4ISz1z3FxwJbRm2xWZ9eE1KrreSgJQdoQb8m9zfk7RaYsfyo8W-tAGt1~bdJuEHgz20HC44anqDqSk6Vo37jiU0H89X1kMRJCST08rD9oVz2gQGMEY40R~9vtWtK42k3Ia965BpUm4ZJvv2bLkmkOeL~tf8QoB5hy2KiZPObPGnjuce8qARBsBciA5o3oCev~Q__" alt="" />
+ <h2 className='t-p' >Creates space from your phone</h2>
+ <p className='p-tt' >Unlike other solutions designed to reduce screen time, the Aro platform combines an app and a physical device to create distance between you and your phone so you can have quality time without the distraction of your phone.</p>
+  </div>
+  <div className="main-over-two">
+    <img  className='img-over' src="https://s3-alpha-sig.figma.com/img/7a26/5d23/f45951c013d554cf79a3b27d9f9ab8b6?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jJbv5y2DR9fiRSScKrj-LZHUYJj6ucYlxkSI6IhB0WlrTOwPtkSqlwnmE-J1OCkKXvVpyPBP8y2X5uFUON6DehOkgqjB6noID99bakzhFv~rldBqo8zHV3kYb-c0z9briQLFyUvMi971fY7kF4wYB5bRHI-H-7wmFzRpB4NHrzG6A6gWf-0MEpW4WA2DMB3uErBqftW00knh5h5tPuTNTdnKwQMVeG~kOqoRGBx3Sm96~FdgAFoWeIgcYyRLv7D2Pt3PzUHRmlKCCbFiORrEcGbg~1hG4nFqVVTu4vkA~tZHgkFLlENvywHxaGh5uQc3bhaAzPQm9Ga0iaYmKfE1Gg__" alt="" />
+ <h2 className='t-p' >Creates space from your phone</h2>
+ <p className='p-tt' >Unlike other solutions designed to reduce screen time, the Aro platform combines an app and a physical device to create distance between you and your phone so you can have quality time without the distraction of your phone.</p>
+  </div>
+  <div className="main-over-two">
+    <img  className='img-over' src="https://s3-alpha-sig.figma.com/img/380a/6e2a/f916be9d1f970bf2f5232a5817d5604e?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MNO9mi-JWJ3eX3oij9X0g1tdSIouifnknGc8cuWDFScciJory-fBNPM5pdVqUTqmiPvEsHP9i0cXdihFGu65pMRGDHESnNQRuzbKRV2mBE09Ni7sd~pXGdakgY1WMrIutMLPixyceoeQMCzo53knK0hEMHMAymcxc0geVj-Y2iXXx80z5eNEW0W8~FYV4Hvw-ZGBykxICJjxNGqLEc-AI7hANoFwzEVC5iyxRyHErHUupyDg9q8lUDIUZEsUvFwAQ34L75z7T42ZdiZ0L0gCMkV8cLfGhRwt7ii1~R2SaTHPnCtM0cfIQAsnVlKsBQ1zMySomYCwnvVAnQYpLP4L2w__" alt="" />
+ <h2 className='t-p' >Creates space from your phone</h2>
+ <p className='p-tt' >Unlike other solutions designed to reduce screen time, the Aro platform combines an app and a physical device to create distance between you and your phone so you can have quality time without the distraction of your phone.</p>
+  </div>
+</div>
+<h1 className='i' >89% of interactions with our smartphones are self-initiated.</h1>
+<p className='s' > Source: Dr. Maxi Heitmayer</p>
+<h1 className='o' >of people can use their phone when it's not in their hand.</h1>
+<div className="main-finish">
+  <div className="main-f">
+    <img className='img-f' src="https://s3-alpha-sig.figma.com/img/7e7e/1ea4/85df72e19f44d7939e34cb540d2d9595?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Rf6c2SIPL05943FkGrtlIPCK5Di1PbXobdOj71AWVXQ9hFPg5Y-AqYmvaFenjT10h0ptHXHQNHkVSjvBZE49vyD27-ZDRtGfvfhd1I5TZtLaBrmtQczMF3hHwfTPST5oGOlqCqjM6gzIy1fKKdQPeRZojAqRloHcftjZHYdIKHFqxB81tbN~Xjfe5aXueT1SVI99yWd9oF9AxcutDq~CHldqeqddtwnZFgSI4SdCwGfqY5wKo5ohkjtpb6CzpN00paJ52DJOpUgeBOalVMB7rvVGSma3EEykueE-QJKO00QSLfegbNOOMruRknpnJzi5lb7DHClZDq~KnFuOKY5K~Q__" alt="" /> <br />
+  <img className='img-f' src="https://s3-alpha-sig.figma.com/img/01f3/46cd/a4d9c0f9804e175a8c6e1c75dc1485d4?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=T8J9K2jhonW7pNwjo6hzhO1wRz~htYFgBF8qeiJptAHisUfPqswKfERGX5EFBCPQ7pAb3vniFivVxXf2qiAWWQ6qMvMZe70xew~VfluYsbo~7T6hKT9nq8mrio7In6YEQTVZHl8HI4BUW97DBeIxU2~6DhnlxdeB2q9lexuQTggFWf3UMDeBCjWlyhYyevkWJw9V897kFZxunqJmf69LO0NyrPmiKSW17y3MgDznjJNI5gKnqxoCQf~9OSM2Tph1-hERgewTLQVfG0SAm9vVBv2OrIXy490PDYYiYdcxNHaBCg96nP~FX4t-EBiFBc3aL-2TAeplyMsSGEfpZxsV6w__" alt="" />
+  </div>
+  <div className="main-ff">
+    <h1 className='l' >Life happens  <br />off your phone</h1>
+    <p className='m' >Make phone-free time a part of your daily routine.</p>
+  </div>
+</div>
+<div className="footer">
+  <div className="footer-one">
+    <h1 className='f-o' >Order your Aro</h1>
+    <p className='f-j' >Join Aro now</p>
+    <p className='f-q' >FAQs</p>
+  </div>
+  <div className="footer-two">
+    <h1 className='f-a' >About</h1>
+    <p className='f-oo' >Our story</p>
+    <p className='f-bb' >Blog</p>
+  </div>
+  <div className="footer-three">
+    <h1 className='f-c' >Contact</h1>
+    <p className='f-cc' >Contact us</p>
+    <p className='f-ss' >Support</p>
+  </div>
+  <div className="footer-four">
+    <h1 className='f-sss' >Subscribe</h1>
+    <div className="b0x-footer"></div>
+  </div>
+  <img className='img-footer' src="https://s3-alpha-sig.figma.com/img/7009/5a14/10985003155eb52085840806fc249f32?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fjJxf8UP6~aA5Q7ac~3l61BR6QKYbf6hfzDuuYqOxqYkH2t6dLbB5~vjJKuG6DJeiBf3TCYaDfoHx24ltPmG3Hc0CkaEZ71XuxhdwJIKaT9Wa0kxiFzLWg5NW4mNdYK2gsDsZwg6Szq5eiaDpf6bXa3TtYCze9PZ6ku3DaDRvLGbKaAgb~vCMquwXm89e6YOVDMTcRBWoxai2lgXpBE8pwilYo6ELyyo2jkYfYTO-mVh4TBRr~nqP0oFnfAbO3gSX~9E~Hd2tM9T5gzhFHW4xLJVyLsFJGmxyBUwYQEceB53nrN5eeQL7tOq~r5N4F97-Y98r5pDYrwOImp1VmJvPg__" alt="" />
+</div>
+<div className="liner-one"></div>
+<div className="box-footer-one">
+  <div className="boxx-footer">
+    <p className='ppppp' >The Bluetooth® word mark and logos are registered trademarks owned by Bluetooth SIG,Inc. and any use of such marks by Aro is under license. Other trademarks and trade names are those of their respective owners.</p>
+ <h1 className='jjjj' >© 2022 Aro. All rights reserved.</h1>
+  </div>
+  <div className="box-footer-two">
+   <div className="img-boxx"></div>
+  
+   <div className="img-box-one"></div>
+   <div className="img-box-two"></div>
+   <div className="box-img-three"></div>
+   <div className="box-img-five"></div>
+  </div>
+  <h1 className='rr' >Privacy Policy</h1>
+<div className="bbox">
+<h1 className='r' >Terms of Service</h1> <br />
+<h1 className='ee' >Terms of Sale</h1>
+</div>
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
